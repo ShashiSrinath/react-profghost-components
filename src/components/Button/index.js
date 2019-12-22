db26ea4178fcbccfe48e4b086b55b1777d6c2ed2
Shashi Srinath <atshashisrinth@gmail.com>
2019-12-22 14:47:20 +0530

    added shade to button hover and click event

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,51 +1,61 @@
 import React, {useState} from 'react';
 import {useSpring, animated} from 'react-spring'
+import {changeColorShade} from "./util";
 
 const Button = (props) => {
     const [hovered, setHovered] = useState(false);
     const [clicked, setClicked] = useState(false);
 
+    const colors = {
+        background: props.colors && props.colors.background ? props.colors.background : '#5938fb',
+        color: props.colors && props.colors.color ? props.colors.color : '#ffffff',
+        boxShadow:  props.colors && props.colors.boxShadow ? props.colors.boxShadow : '#888888',
+    };
+    colors.hoverBackground = props.colors && props.colors.hoverBackground ? props.colors.hoverBackground : changeColorShade(colors.background , 20);
+    colors.clickedBackground = props.colors && props.colors.clickedBackground ? props.colors.clickedBackground : changeColorShade(colors.background , -20);
+
     const btnStyles = {
         padding: '10px 20px',
-        background: '#5938fb',
-        color: 'white',
+        color: colors.color,
         border: 'none',
         outline: 'none',
         cursor: hovered ? 'pointer' : 'auto',
     };
 
     const hoverAnimation = useSpring({
-        transform: hovered ? clicked ? 'translateZ(0) scale(1.1)' : 'translateZ(0) scale(1.2)' : 'translateZ(0) scale(1)',
-        borderRadius: hovered ? '15px' : '5px',
-        boxShadow: hovered ? clicked ? '3px 3px 8px #888888' : '5px 5px 8px #888888' : '1px 1px 8px #888888',
+        transform: hovered ? clicked ? 'scale(1.1)' : 'scale(1.25)' : 'scale(1)',
+        borderRadius: hovered ? clicked ? '10px' : '15px' : '5px',
+        boxShadow: hovered ? clicked ? `3px 3px 8px ${colors.boxShadow}` : `5px 5px 8px ${colors.boxShadow}` : `1px 1px 8px ${colors.boxShadow}`,
+        background: hovered ? clicked ? colors.clickedBackground : colors.hoverBackground : colors.background,
     });
 
     return (
-            <animated.button
-                style={{...btnStyles, ...props.styles, ...hoverAnimation}}
-                className={props.className}
+        <animated.button
+            style={{...btnStyles, ...props.styles, ...hoverAnimation}}
+            className={props.className}
 
-                {...props.events}
-                onClick={props.onClick ? props.onClick : props.events && props.events.onClick ? props.events.onClick : undefined}
-                onMouseEnter={() => {
-                    setHovered(true);
-                    if (props.events && props.events.onMouseEnter) props.events.onMouseEnter();
-                }}
-                onMouseOut={() => {
-                    setHovered(false);
-                    if (props.events && props.events.onMouseOut) props.events.onMouseOut();
-                }}
-                onMouseDown={() => {
-                    setClicked(true);
-                    if (props.events && props.events.onMouseDown) props.events.onMouseDown();
-                }}
-                onMouseUp={() => {
-                    setClicked(false);
-                    if (props.events && props.events.onMouseUp) props.events.onMouseUp();
-                }}
-            >
-                {props.children}
-            </animated.button>
+            {...props.events}
+            onClick={props.onClick ? props.onClick : props.events && props.events.onClick ? props.events.onClick : undefined}
+            onMouseEnter={() => {
+                setHovered(true);
+                if (props.events && props.events.onMouseEnter) props.events.onMouseEnter();
+            }}
+            onMouseOut={() => {
+                setHovered(false);
+                setClicked(false);
+                if (props.events && props.events.onMouseOut) props.events.onMouseOut();
+            }}
+            onMouseDown={(e) => {
+                setClicked(true);
+                if (props.events && props.events.onMouseDown) props.events.onMouseDown();
+            }}
+            onMouseUp={(e) => {
+                setClicked(false);
+                if (props.events && props.events.onMouseUp) props.events.onMouseUp();
+            }}
+        >
+            {props.children}
+        </animated.button>
     );
 };
 
